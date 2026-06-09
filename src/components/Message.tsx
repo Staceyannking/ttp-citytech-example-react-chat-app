f11d702071b352e8cdc8879a812403a85fe830ac
Staceyann King <staceyannking@Staceyanns-MacBook-Air.local>
2026-06-09 10:34:30 -0400

    add time to message.tsx'

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -4,13 +4,16 @@ import { faker } from "@faker-js/faker";
 const Message = ({text}) => {
     //BUG: the name regenerates on every new message. old message should retain their name 
     const name = faker.person.fullName();
+    const time = faker.person.time()
 
     return <>
     <p className='message'>
-        {text} - {name}
+        {text} - {name}- [time]
     </p>
     </>;
 
 }
+
+
 
 export default Message
